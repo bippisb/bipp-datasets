38d1e44b31a880bebded6472190dbf8a29a6f13e
Shravani <Shravani_Sonawane@isb.edu>
2023-12-14 10:49:46 +0530

    updated parse enrollment.py

--- projects/udise/notebooks/parse_enrollment_details.py
+++ projects/udise/notebooks/parse_enrollment_details.py
@@ -133,26 +133,36 @@ def parse_enrollment_by_age(page_2: pd.DataFrame) -> pd.DataFrame:
 
 # %%
 
-def extract_and_parse_teacher_qual(page_1: pd.DataFrame):
-    teacher_qual_table = extract_table_between_keywords(
-        page_1, "Teacher With Profes", "Diploma/degree in special Educatio"
-    )
+def extract_and_parse_teacher_qual(page_1: pd.DataFrame, start_keyword, end_keyword):
+    start_pattern = generate_pattern(start_keyword)
+    end_pattern = generate_pattern(end_keyword)
+    # Find the row numbers of start and end keywords using regex
+    start_rows = page_1[page_1.apply(lambda row: bool(
+        start_pattern.search(str(row.values))), axis=1)].index
+    end_rows = page_1[page_1.apply(lambda row: bool(
+        end_pattern.search(str(row.values))), axis=1)].index
+    if start_rows.empty:
+        return None
+    # get row indexes of start and end keywords
+    start_idx = start_rows.min()
+    end_idx = end_rows.max() + 1 if not end_rows.empty else len(page_1)
+    assert end_idx > start_idx, "End index must be greater than start index"
+    teacher_qual_table = page_1.loc[start_idx:end_idx].copy()  # Create a copy to avoid the SettingWithCopyWarning
     # Check if the teacher_qual_table is not empty
     if teacher_qual_table.empty:
         return None
     # Reset index for proper alignment
     teacher_qual_table.reset_index(drop=True, inplace=True)
-    # Extract values using a modified regular expression to split the string
-    split_values = teacher_qual_table.iloc[1:, 10].str.extract(
-        r'(\d+)\s*(\D.*)', expand=True)
+    # Extract values using a modified regular expression to split the string 
+    split_values = teacher_qual_table.iloc[1:, 10].str.extract(r'(\d+)\s*(\D.*)', expand=True)
     # Assign the correct values to columns
     teacher_qual_table.loc[1:, 1] = split_values[0]
     teacher_qual_table.loc[1:, 2] = split_values[1]
-    final_table = pd.concat([teacher_qual_table[0], split_values[0],
-                            split_values[1], teacher_qual_table[19]], axis=1)
+    final_table = pd.concat([teacher_qual_table[0], split_values[0], split_values[1], teacher_qual_table[19]], axis=1)
     final_table = final_table.iloc[1:-1].reset_index(drop=True)
     final_table.columns = [0, 1, 2, 3]
     return final_table
+
 
 def students_enrolled_under_section_12(page_1: pd.DataFrame):
     table = extract_table_between_keywords(page_1, "Total no. of Students Enrolled Under Section 12 of the RTE Act In Private Unaided and Specified Category Schools (DCF Sl. No. 1.42(a))", "Total no. of Economically Weaker Section*(EWS) students Enrolled in Schools (DCF Sl. No. 1.42(b))")
@@ -186,8 +196,12 @@ def teachers(page_1: pd.DataFrame):
     table.iloc[:, 20] = split_values[0]
     table.iloc[:, 21] = split_values.iloc[:, 1]
     value_to_cut = table.iloc[6, 2]
-    table.iloc[6, 2] = ' ' 
-    table.iloc[6, 3] = value_to_cut
+    # Create a copy of the subset to avoid SettingWithCopyWarning
+    subset = table.iloc[6:7].copy()
+    subset.iloc[:, 2] = ' '
+    subset.iloc[:, 3] = value_to_cut
+    # Update the original DataFrame with the modified subset
+    table.iloc[6:7] = subset
     return table
 
 
