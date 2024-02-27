029ca67640aec95d49490868adda4cbff7008b71
kkhatke <karan.khatke@gmail.com>
2024-02-27 20:11:44 +0530

    RBI Money stocks script

---
+++ projects/rbi-money-stocks/src/data/get_raw_data.py
@@ -0,0 +1,45 @@
+# import libraries
+import os
+
+import requests
+from lxml import html
+
+# set paths
+dir_path = os.path.dirname(os.path.dirname(os.getcwd()))
+data_path = os.path.join(dir_path, "data")
+raw_path = os.path.join(data_path, "raw")
+
+# set url
+url = "https://m.rbi.org.in/scripts/WSSViewDetail.aspx?TYPE=Section&PARAM1=7"
+
+# get data links from website
+main_session = requests.Session()
+response = main_session.get(url)
+tree = html.fromstring(response.content)
+
+xls_links = [
+    a
+    for a in tree.xpath('//table[@class="tablebg"]//td//a/@href')
+    if a.endswith("XLSX")
+]
+len_links = len(xls_links)
+dates = [a for a in tree.xpath('//table[@class="tablebg"]//tr//th/text()')][
+    :len_links
+]
+main_session.close()
+
+# Create a dictionary with dates as keys and links as values
+date_link_dict = dict(zip(dates, xls_links))
+
+# Download the files
+for date, link in date_link_dict.items():
+    session = requests.Session()
+    print(f"Downloading {link}")
+    resp = session.get(link)
+    # Format the filename based on the date
+    filename_date = date.replace(" ", "_")
+    local_excel_path = os.path.join(raw_path, f"{filename_date}.xlsx")
+    with open(local_excel_path, "wb") as f:
+        f.write(resp.content)
+    print(f"Saved to {local_excel_path}")
+    session.close()
---
+++ projects/rbi-money-stocks/src/data/process_data_dbie.py
@@ -0,0 +1,84 @@
+# %%
+import os
+
+import pandas as pd
+
+# %%
+# set paths
+dir_path = os.path.dirname(os.path.dirname(os.getcwd()))
+data_path = os.path.join(dir_path, "data")
+raw_path = os.path.join(data_path, "raw")
+interim_path = os.path.join(data_path, "interim")
+processed_path = os.path.join(data_path, "processed")
+# %%
+# get file paths from raw folder
+local_files = [
+    os.path.join(raw_path, f)
+    for f in os.listdir(raw_path)
+    if f.endswith(".xlsx")
+]
+# %%
+file = local_files[0]
+# read excel file
+df = pd.read_excel(file, engine="openpyxl")
+# %%
+# drop columns with all NaN
+df = df.dropna(how="all", axis=1)
+# %%
+# find the row index where row values contains 'Date'
+date_row = df[df.apply(lambda row: "Date" in row.values, axis=1)].index[0]
+# %%
+# concate the date_row and date_row+1 and assign them as column names
+df.columns = (
+    df.iloc[date_row].fillna("") + " " + df.iloc[date_row + 1].fillna("")
+).values.tolist()
+# %%
+# drop columns where 'merger' is present in the column name
+df = df[[col for col in df.columns if "merger" not in col.lower()]]
+# %%
+# select rows from date_row+2 to the last row-2
+df = df.iloc[date_row + 2 : -2]
+# %%
+col_names = {
+    "Date ": "date",
+    "M3 ": "m3",
+    "1 Components (1.1.+1.2+1.3+1.4) 1.1 Currency with the Public": "currency_with_public",
+    " 1.2 Demand Deposits with Banks": "demand_deposits_with_banks",
+    " 1.3 Time Deposits with Banks": "time_deposits_with_banks",
+    " 1.4 ‘Other’ Deposits with Reserve Bank": "other_deposits_with_rbi",
+    "2 Sources (2.1+2.2+2.3+2.4-2.5) 2.1 Net Bank Credit to Government": "2.1",
+    " 2.1.1 Reserve Bank": "reserve_bank_crd_to_gov_src",
+    " 2.1.2 Other Banks": "other_banks_crd_to_gov_src",
+    " 2.2 Bank Credit to Commercial Sector": "2.2",
+    " 2.2.1 Reserve Bank": "reserve_bank_crd_to_comm_sctr_src",
+    " 2.2.2 Other Banks": "other_banks_crd_to_comm_sctr_src",
+    " 2.3 Net Foreign Exchange Assets of Banking Sector": "net_foreign_exchange_assets_src",
+    " 2.4 Government's Currency Liabilities to the Public": "gov_currency_liab_to_public_src",
+    " 2.5 Banking Sectors Net Non-Monetary Liabilities": "net_non_monetary_liab_bnk_sctr_src",
+    " 2.5.1 Net Non-Monetary Liabilities of RBI": "net_non_monetary_liab_rbi_src",
+    " ": "2.6",
+}
+df.rename(columns=col_names, inplace=True)
+# %%
+# drop column 2.1, 2.2 and 2.6
+df = df.drop(columns=["2.1", "2.2", "2.6"])
+# %%
+# convert all columns to numeric except date column
+for c in df.columns[1:]:
+    df[c] = pd.to_numeric(df[c])
+# %%
+# convert date column to datetime
+df["date"] = pd.to_datetime(df["date"]).dt.strftime("%d-%m-%Y")
+# %%
+date = str(df["date"].iloc[0])
+# %%
+# Save as csv file in processed folder
+processed_csv_path = os.path.join(
+    processed_path, f"dbie_money_stocks_{date}.csv"
+)
+df.to_csv(processed_csv_path, index=False)
+# Save as parquet file in processed folder
+processed_parquet_path = os.path.join(
+    processed_path, f"dbie_money_stocks_{date}.parquet"
+)
+df.to_parquet(processed_parquet_path, index=False)
